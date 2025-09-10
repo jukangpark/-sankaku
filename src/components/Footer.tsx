569fc41b2547f2b239ece767677a05747a74c256
주강 <jkpark@nkia.co.kr>
2025-09-10 16:06:39 +0900

    fix: 2024 -> 2025 All rights reserved. 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -84,7 +84,7 @@ const Footer = () => {
         </div>
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 산카쿠. All rights reserved.</p>
+          <p>&copy; 2025 산카쿠. All rights reserved.</p>
         </div>
       </div>
     </footer>
